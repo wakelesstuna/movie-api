bd9b090e8042c8011002a6b0ca902d0a9ac57a66
oscar.forss <oscar.forss@globalconnect.se>
2023-04-24 15:14:20 +0200

    first commit

---
+++ src/test/java/net/wakelesstuna/movie/MovieApplicationTests.java
@@ -0,0 +1,13 @@
+package net.wakelesstuna.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
